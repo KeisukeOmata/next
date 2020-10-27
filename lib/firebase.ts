ac107007e2fcec038f13a147d57893b28908289a
小俣圭佑 <omatakeisuke@MacBook.local>
2020-10-27 21:40:31 +0900

    init

---
+++ lib/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+if (typeof window !== 'undefined' && firebase.apps.length === 0) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyC153dApxFJGKp0YGKYwAGBerHw5GNgaoM",
+    authDomain: "next-57fd0.firebaseapp.com",
+    databaseURL: "https://next-57fd0.firebaseio.com",
+    projectId: "next-57fd0",
+    storageBucket: "next-57fd0.appspot.com",
+    messagingSenderId: "884347730579",
+    appId: "1:884347730579:web:4f94c221c14352a2b32b66",
+    measurementId: "G-F5PJKYMCER"
+  }
+
+  firebase.initializeApp(firebaseConfig)
+  firebase.analytics()
+}
