478120eae4580a5e091d66ad08d73b54f6d2444f
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-01 13:22:45 +0900

    [add] formを追加

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import firebase from 'firebase/app'
 import { useEffect, useState } from 'react'
 // URLにパラメータを動的に含めてアクセス
@@ -8,16 +9,23 @@ import Layout from '../../components/Layout'
 
 export default function UserShow() {
   const router = useRouter()
+  // ユーザーのstate
   // グローバルに管理する必要がないため、RecoilではなくuseState
   const [user, setUser] = useState<User>(null)
   const query = router.query as Query
+  // フォームのstate
+  const [body, setBody] = useState('')
+  // ボタンのstate
+  const [isSending, setIsSending] = useState(false)
 
+  // query.uidが変更された場合firebaseからユーザーの読み込みを行う
   useEffect(() => {
     // queryに値がある場合だけ処理する
     if (query.uid === undefined) {
       return
     }
 
+    // firebaseからユーザーの読み込みを行う
     async function loadUser() {
       const doc = await firebase
         .firestore()
@@ -31,6 +39,7 @@ export default function UserShow() {
   
       const gotUser = doc.data() as User
       gotUser.uid = doc.id
+      // ユーザーのstateにfirebaseから取得した値を設定
       setUser(gotUser)
     }
 
@@ -38,12 +47,65 @@ export default function UserShow() {
   // query.uidが変更される度に呼び出す
   }, [query.uid])
   
+  // フォームのsubmitで呼ばれる
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    
+    // ボタンのstateを変更
+    setIsSending(true)
+    // firebaseにaddする
+    await firebase.firestore().collection('questions').add({
+    senderUid: firebase.auth().currentUser.uid,
+    receiverUid: user.uid,
+    // フォームのstate
+    body,
+    isReplied: false,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    // ボタンのstateを戻す
+    setIsSending(false)
+  
+    // フォームのstateを初期化
+    setBody('')
+    alert('質問を送信しました。')
+  }
+
   return (
     <Layout>
       {user && (
         <div className="text-center">
           <h1 className="h4">{user.name}さんのページ</h1>
           <div className="m-5">{user.name}さんに質問しよう！</div>
+          {/* フォーム */}
+          {/* justify-content-centerでcolの中央寄せ */}
+          <div className="row justify-content-center mb-3">
+            {/* モバイルは横幅いっぱい、PCはcontainerの半分 */}
+            <div className="col-12 col-md-6">
+              <form onSubmit={onSubmit}>
+              <textarea
+                className="form-control"
+                placeholder="おげんきですか？"
+                rows={6}
+                // formの入力値でstateを更新
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                required
+              ></textarea>
+              <div className="m-3">
+                {isSending ? (
+                  // ボタンのstateがtrueの場合アニメーションを表示する
+                  <div className="spinner-border text-secondary" role="status">
+                  </div>
+                ) : (
+                  // ボタンのstateがfalseの場合ボタンを表示する
+                  <button type="submit" className="btn btn-primary">
+                    質問を送信する
+                  </button>
+                )}
+              </div>
+              </form>
+            </div>
+          </div>
         </div>
       )}
     </Layout>
