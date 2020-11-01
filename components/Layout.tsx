97b8ab6470596910eb51ec235209548d333550af
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-01 10:44:52 +0900

    [add] Bootstrapを追加

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg navbar-light mb-3"
+        style={{ backgroundColor: '#e3f2fd' }}
+      >
+        {/* Bootstrapをあてる箇所をcontainerで囲む */}
+        <div className="container">
+          <div className="mr-auto">
+            <a className="navbar-brand" href="#">
+              Navbar
+            </a>
+          </div>
+          <form className="d-flex">
+            <button className="btn btn-outline-primary" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <div className="container">{children}</div>
+    </div>
+  )
+}
