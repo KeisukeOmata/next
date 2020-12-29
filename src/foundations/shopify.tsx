9a5e8ded5dbb7652b8dbaa4989df559d4fa52237
小俣圭佑 <omatakeisuke@MacBook.local>
2020-12-29 17:46:11 +0900

    Can't use environment variables in shopify-buy?

--- src/foundations/shopify.tsx
+++ src/foundations/shopify.tsx
@@ -1,7 +1,7 @@
 import Client from 'shopify-buy'
 
 export const shopify = Client.buildClient({
-  storefrontAccessToken: process.env.API_KEY as string,
-  domain: process.env.DOMAIN as string,
+  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
+  domain: 'graphql.myshopify.com',
   language: 'ja-JP',
 })
