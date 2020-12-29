ed4fc366ce74bd89316772213c1fbcffe02bc2aa
小俣圭佑 <omatakeisuke@MacBook.local>
2020-12-30 05:53:27 +0900

    Add header content

--- src/components/layouts/SiteHeader.tsx
+++ src/components/layouts/SiteHeader.tsx
@@ -13,22 +13,37 @@ const Header: React.FC = () => {
         <div className={styles.siteHeader__inner}>
           <Link href="/" passHref>
             <a className={styles.siteHeader__logoLink}>
-              <img
-                src="/logo.svg"
-                // スクリーンリーダーでWebページを読む人のためにimgのalt属性を指定する
-                // 不要な画像にも alt="" を設定する
-                // SVGの場合はaria-label属性を指定、画像として使う場合は role="img" を指定
-                // SVGをボタンとして使う場合はbuttonタグで囲い、aria-label属性もbuttonで指定
-                // <button><svg role="img" aria-label="説明"></svg></button>
-                alt={Config.siteMeta.title}
-                className={styles.siteHeader__logoImg}
-              />
+              <button>SKPISM</button>
             </a>
           </Link>
+          <div className={styles.siteHeader__links}>
+            {Config.headerLinks.map((link, i) => {
+              // keyを作成
+              const key = `header-link-${i}`
+              // サイト内リンクの場合
+              if (link.href.startsWith('/')) {
+                return (
+                  <Link key={key} href={link.href} passHref>
+                    <a className={styles.siteHeader__link}>{link.title}</a>
+                  </Link>
+                )
+              }
+              // サイト外リンクの場合
+              return (
+                <a
+                  key={key}
+                  href={link.href}
+                  className={styles.siteHeader__link}
+                >
+                  {link.title}
+                </a>
+              )
+            })}
+          </div>
           <Link href={'/cart'} passHref>
-            <div>
+            <a>
               <button>BAG({quantity})</button>
-            </div>
+            </a>
           </Link>
         </div>
       </ContentWrapper>
