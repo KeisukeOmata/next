be65860ca2337cf4e44b036c33fa232dc5d45a89
小俣圭佑 <omatakeisuke@MacBook.local>
2021-01-03 18:00:52 +0900

    Change the image size in the cart

--- src/components/cart/CartItems.tsx
+++ src/components/cart/CartItems.tsx
@@ -33,8 +33,8 @@ const CartItems: React.FC = () => {
                   <Image
                     src={item.variant.image.src}
                     alt={item.variant.image.altText ?? ''}
-                    width={300}
-                    height={300}
+                    width={500}
+                    height={500}
                   />
                 </div>
                 <div className={styles.item}>
