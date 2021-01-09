cc7cb75fd90edb5724d387d4fdbbd862ba44b475
小俣圭佑 <omatakeisuke@MacBook.local>
2021-01-09 17:17:58 +0900

    fix

--- src/foundations/shopify.tsx
+++ src/foundations/shopify.tsx
@@ -1,7 +1,7 @@
 import Client from 'shopify-buy'
 
 export const shopify = Client.buildClient({
-  storefrontAccessToken: '998272c4d685c2c33ed1e8095ceaf5ba',
-  domain: 'omatakeisuke.myshopify.com',
+  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
+  domain: 'graphql.myshopify.com',
   language: 'ja-JP',
 })
