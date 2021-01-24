5cd1efac0a90389983215b4f03973af5ff90600d
小俣圭佑 <omatakeisuke@MacBook.local>
2021-01-25 08:22:07 +0900

    Change to focus on the item when browser back

--- src/components/items/Items.tsx
+++ src/components/items/Items.tsx
@@ -1,21 +1,35 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useSetRecoilState } from 'recoil'
+import { itemAtom } from './CategoryItems'
 import { TypeItem } from '../../types/TypeItem'
 import styles from '../../styles/components/items/Items.module.scss'
 
 type Props = {
   item: TypeItem
+  focused: boolean
 }
 
-const Items: FC<Props> = ({ item }) => {
+const Items: FC<Props> = ({ item, focused }) => {
   const { id, images, title, variants } = item
+  const ref = useRef<HTMLButtonElement>()
+  const setItemState = useSetRecoilState(itemAtom)
+
+  useEffect(() => {
+    if (ref.current && focused) {
+      ref.current.focus()
+    }
+  }, [focused])
 
   return (
     <>
       <div className={styles.item}>
         <Link key={`items-${id}`} href={`items/${id}`} passHref>
           <button
+            ref={ref as any}
+            tabIndex={-1}
+            onClick={() => setItemState(id as string)}
             aria-label={`${title}のページを表示する`}
             className={styles.item__mainLink}
           >
