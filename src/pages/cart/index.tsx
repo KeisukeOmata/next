f0c29a7e8570f348b57caa57cfba3360ffe9d6ff
小俣圭佑 <omatakeisuke@MacBook.local>
2021-01-30 20:16:41 +0900

    Add PageSEO for pages/cart/index.tsx

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import CartItems from '../../components/cart/CartItems'
+import { PageSEO } from '../../components/layouts/PageSEO'
 import { ContentWrapper } from '../../components/layouts/ContentWrapper'
 import { useCart } from '../../hooks/useCart'
 import s from '../../styles/pages/cart/index.module.scss'
@@ -9,6 +10,7 @@ export default function Cart() {
   fetchCart()
   return (
     <>
+      <PageSEO title="Cart" path="/cart" />
       <section className={s.cartCategories}>
         <ContentWrapper>
           {cart === null ? <div>loading...</div> : <CartItems />}
