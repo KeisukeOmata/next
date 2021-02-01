bdca9cb88695215795fd5afca3218888e01624d1
小俣圭佑 <omatakeisuke@MacBook.local>
2021-02-02 08:18:04 +0900

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   },
   purge: {
     content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}',
     ],
   },
   theme: {
