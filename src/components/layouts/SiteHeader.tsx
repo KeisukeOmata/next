96e7d542d4a8180080632eebf8b30439703ccba3
小俣圭佑 <omatakeisuke@MacBook.local>
2021-02-10 08:49:01 +0900

    Add dark mode icon

--- src/components/layouts/SiteHeader.tsx
+++ src/components/layouts/SiteHeader.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { FC } from 'react'
 import { ContentWrapper } from 'components/layouts/ContentWrapper'
+import { DarkMode } from 'components/ui/DarkMode'
 import { useCart } from 'lib/hooks/useCart'
 import s from 'styles/components/layouts/SiteHeader.module.scss'
 
@@ -26,6 +27,8 @@ const Header: FC = () => {
             <Link href={'/world'} passHref>
               <button aria-label="about usを表示する">World</button>
             </Link>
+            <div className={s.slash}>/</div>
+            <DarkMode />
           </div>
           <Link href={'/cart'} passHref>
             <button aria-label="カートを表示する">BAG({quantity})</button>
