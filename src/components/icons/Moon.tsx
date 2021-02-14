7ea07185cd8b151756cb744d6b3ed29c72c3bf71
小俣圭佑 <omatakeisuke@MacBook.local>
2021-02-15 07:56:09 +0900

    Remove unnecessary returns

--- src/components/icons/Moon.tsx
+++ src/components/icons/Moon.tsx
@@ -1,20 +1,18 @@
 import { FC } from 'react'
 
-export const Moon: FC = () => {
-  return (
-    <svg
-      aria-label="月のアイコン"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      fill="none"
-      shapeRendering="geometricPrecision"
-    >
-      <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
-    </svg>
-  )
-}
+export const Moon: FC = () => (
+  <svg
+    aria-label="月のアイコン"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    fill="none"
+    shapeRendering="geometricPrecision"
+  >
+    <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+  </svg>
+)
