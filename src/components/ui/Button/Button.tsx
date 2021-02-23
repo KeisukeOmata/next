8de181bfa9b4c2bba4dcc4954fcc204b9f46d36b
小俣圭佑 <omatakeisuke@MacBook.local>
2021-02-24 06:08:06 +0900

    Remove import react

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,8 +1,8 @@
-import { ButtonHTMLAttributes } from 'react'
+import { FC, ButtonHTMLAttributes } from 'react'
 import s from './Button.module.css'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
-export const Button: React.FC<ButtonProps> = ({ ...rest }) => (
+export const Button: FC<ButtonProps> = ({ ...rest }) => (
   <button className={s.button} {...rest}></button>
 )
